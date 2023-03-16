29cea12dec698f2b392dde9cfb8ab690ab46ba7c
BenitoSwaggolini <profie_player@mail.ru>
2023-03-16 18:59:21 +0500

    realized db geocoords save, fixed legacy code

--- main.py
+++ main.py
@@ -7,17 +7,28 @@ from requests import Session
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.poolmanager import PoolManager
 from requests.packages.urllib3.util import ssl_
+from fastapi.responses import HTMLResponse
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
+import sqlite3
 
 from set import API_KEY
 
+templates = Jinja2Templates(directory='templates')
 app = FastAPI()
-
+con = sqlite3.connect("markers.db", check_same_thread=False)
+cur = con.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS map_markers (
+address VARCHAR(100),
+coords VARCHAR(100) )
+""")
 cj = browser_cookie3.edge()
 
 CIPHERS = """ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:AES256-SHA"""
 
 
 class TlsAdapter(HTTPAdapter):
+    """Create adapter to go through block from site"""
 
     def __init__(self, ssl_options=0, **kwargs):
         self.ssl_options = ssl_options
@@ -29,50 +40,75 @@ class TlsAdapter(HTTPAdapter):
 
 
 def create_session() -> Session:
+    """Create requests session"""
     session = requests.session()
     adapter = TlsAdapter(ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1)
     session.mount("https://", adapter)
     return session
 
 
-def take_favorites_from_page(session, url) -> list | None:
+def get_data_from_current_page(session, page: int) -> list | None:
+    """Take favorite products and data about them from page"""
+    url = f'https://www.avito.ru/web/1/favorites/items/list?order=added_at__desc&page={page}'
     data = session.request('GET', url, cookies=cj)
     try:
         favorites = data.json()["items"]
+        if not favorites:
+            return None
+        return [get_important_data_from_curr_product(product) for product in favorites]
     except KeyError:
         return None
-    return favorites
 
 
 def get_important_data_from_curr_product(product: dict) -> dict:
-    coords: dict = get_geocoords(product["address"])
+    """Take information about the product"""
     item_link: str = "https://www.avito.ru" + product["uri"]
     title: str = product["title"]
-
+    coords: dict = get_geocoords(product["address"])
     return {"coords": coords, "item_link": item_link, "title": title}
 
 
 def get_geocoords(address: str) -> dict:
-    link = f"https://geocode-maps.yandex.ru/1.x/?apikey={API_KEY}&format=json&geocode={address}"
-    coords = requests.get(link).json()["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]
-    x, y = coords["pos"].split()[::-1]
+    """Get geocoords from DB or yandex geocoding API"""
+    res = cur.execute(f"""SELECT coords from map_markers WHERE address='{address}'""").fetchall()
+    if not res:
+        link = f"https://geocode-maps.yandex.ru/1.x/?apikey={API_KEY}&format=json&geocode={address}"
+        coords = requests.get(link).json()["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]
+        x, y = coords["pos"].split()[::-1]
+
+        """Save in db"""
+        db_x_y_look = str(x) + ' ' + str(y)
+        cur.execute(f"""INSERT INTO map_markers VALUES 
+        ('{address}', '{db_x_y_look}')""")
+        con.commit()
+        return {"x": x, "y": y}
+
+    x, y = res[0][0].split()
     return {"x": x, "y": y}
 
 
 session = create_session()
 
 
-@app.get('/avito/{page}')
-async def get_data_from_curr_avito_page(page: int):
-    url = f'https://www.avito.ru/web/1/favorites/items/list?order=added_at__desc&page={page}'
-    data = take_favorites_from_page(session=session, url=url)
-    if not data:
-        return {}
-    response = [get_important_data_from_curr_product(product) for product in data]
-    return response
+@app.get('/get_products/')
+def get_all_products() -> list:
+    """Take all data from avito favorites"""
+    page = 2
+    products = []
+    while True:
+        response = get_data_from_current_page(session=session, page=page)
+        if not response:
+            break
+
+        #  page += 1
+        products.extend(response)
+        break
+    return products
 
 
-
+@app.get('/', response_class=HTMLResponse)
+async def map_page(request: Request):
+    return templates.TemplateResponse("placemark.html", {"request": request, "API_KEY": API_KEY})
 
 
 if __name__ == '__main__':
